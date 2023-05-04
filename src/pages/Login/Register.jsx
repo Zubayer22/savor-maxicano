04da2b2a0c3fa16aaf49242f9d587b45b36865c0
zubayer22 <zubayerkh@gmail.com>
2023-05-04 14:51:22 +0600

    PhotoURL issue solve for header

--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Providers/AuthProvider';
+import { updateProfile } from 'firebase/auth';
 
 
 const Register = () => {
@@ -17,16 +18,25 @@ const Register = () => {
         event.preventDefault();
         const form = event.target;
         const name = form.name.value;
-        const photURL = form.photo.value;
+        const photoURL = form.photo.value;
         const email = form.email.value;
         const password = form.password.value;
 
-        console.log(name, photURL, email, password);
+        console.log(name, photoURL, email, password);
 
         createUser(email, password)
             .then(result => {
                 const createdUser = result.user;
                 console.log(createdUser);
+
+                // Update the user's display name and photo URL
+                return updateProfile(createdUser, {
+                    displayName: name,
+                    photoURL: photoURL
+                });
+            })
+            .then(() => {
+                // Profile updated successfully
                 logOut();
                 navigate('/login');
             })
@@ -45,11 +55,11 @@ const Register = () => {
             <h1 className='text-center pt-5'>Register Here</h1>
             <Container className='w-50 mx-auto py-5'>
                 <Form onSubmit={handleRegister}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Group className="mb-3" controlId="formBasicName">
                         <Form.Label>Name</Form.Label>
                         <Form.Control type="text" name='name' placeholder="Your Name" required />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Group className="mb-3" controlId="formBasicURL">
                         <Form.Label>Phot URL</Form.Label>
                         <Form.Control type="text" name='photo' placeholder="Your Photo URL" required />
                     </Form.Group>
@@ -69,10 +79,10 @@ const Register = () => {
                         Register
                     </Button>
                     <Form.Group>
-                        
+
                         <Form.Text className='text-danger'>
                             {error}
-                        </Form.Text><br/>
+                        </Form.Text><br />
                         <Form.Text className='text-secondary'>
                             Already have an account? <Link to='/login'>Login</Link>
                         </Form.Text>
